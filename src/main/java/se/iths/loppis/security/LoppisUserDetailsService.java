f9256c28a9adec694184c15d42c1e744601ab9d0
Pontus Redig <pontus.redig@alten.se>
2020-11-25 10:34:59 +0100

    Added security package + principal

---
+++ src/main/java/se/iths/loppis/security/LoppisUserDetailsService.java
@@ -0,0 +1,27 @@
+package se.iths.loppis.security;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import se.iths.loppis.entity.User;
+import se.iths.loppis.repository.UserRepository;
+
+public class LoppisUserDetailsService implements UserDetailsService {
+
+    private UserRepository userRepository;
+
+    public LoppisUserDetailsService(UserRepository userRepository) {
+        super();
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+        if(user==null) {
+            throw new UsernameNotFoundException("Can't find username: " + username);
+        }
+        return new LoppisUserPrincipal(user);
+    }
+
+}
---
+++ src/main/java/se/iths/loppis/security/LoppisUserPrincipal.java
@@ -0,0 +1,52 @@
+package se.iths.loppis.security;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import se.iths.loppis.entity.User;
+
+import java.util.Collection;
+
+public class LoppisUserPrincipal implements UserDetails {
+
+    private User user;
+
+    public LoppisUserPrincipal(User user) {
+        super();
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return this.user.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
